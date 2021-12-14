cd3e2011eb6b3b440aaf75dc74b4e1f537a69f15
wael9823 <mohd.wael9823@gmail.com>
2021-12-14 15:07:10 -0500

    Visualisation of rotation and translation completely

--- resolver.cpp
+++ resolver.cpp
@@ -12,10 +12,67 @@
 #include <pcl/common/centroid.h>
 #include <pcl/features/moment_of_inertia_estimation.h>
 #include <vector>
+#include <chrono>
+#include <thread>
+
+int viewer_script (std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> simulation_clouds)
+{
+int i =0;
+std::string path= "/home/pinak/PCL/PCD/test";
+  
+    pcl::PointCloud<pcl::PointXYZ>::Ptr ref (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr offsetcloud (new pcl::PointCloud<pcl::PointXYZ>);
+    
+    // Reading stored pointcloud of pointXYZ in cloud variable
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> ("/home/wael/dr_project/Grasp_Assuming_Symmetry/build/testout.pcd", *ref) == -1) //* load the file
+    {
+        PCL_ERROR ("Couldn't read file test3.pcd \n");
+        return (-1);
+    }
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> ("/home/wael/dr_project/Grasp_Assuming_Symmetry/test_pcd.pcd", *cloud) == -1) //* load the file
+    {
+        PCL_ERROR ("Couldn't read file test3.pcd \n");
+        return (-1);
+    }
+
+    std::cout<<"Loaded "<<offsetcloud->width*offsetcloud->height<<"data points from test_pcd3.pcd"<< std::endl;
+    pcl::visualization::PCLVisualizer viewer ("Simple pointcloud display example");
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> ref_cloud_color_handler (ref, 255, 25, 255);
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler (cloud, 255, 20, 25);
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> source_cloud_color_handler4 (offsetcloud, 10, 255, 20);
+    viewer.addPointCloud (cloud, source_cloud_color_handler, "original_cloud");
+    viewer.addPointCloud (ref, ref_cloud_color_handler, "ref_cloud");
+    viewer.addPointCloud (offsetcloud, source_cloud_color_handler4, "original4_cloud");
+    // viewer.addPointCloudNormals<pcl::PointXYZ, pcl::Normal> (cloud, cloud_normals, 10, 0.05, "normals");
+    viewer.addCoordinateSystem (0.5, "cloud", 0);
+    viewer.setBackgroundColor(0.05, 0.05, 0.05, 0); // Setting background to a dark grey
+    // viewer.addSphere(coeff);
+    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "original_cloud");
+    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "ref_cloud");
+    viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "original4_cloud");
+    
+    int count=0;
+    float offset = 0;
+
+    while (!viewer.wasStopped ()) {
+        viewer.spinOnce(2);
+        if(i<simulation_clouds.size()){
+            offsetcloud = simulation_clouds[i];
+            cout<<"PCD"+std::to_string(i)<<endl;
+            viewer.updatePointCloud(offsetcloud,"original4_cloud");
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            i++;
+        }
+
+ }
+return (0);
+}
+
+
 
 // This function displays the help
-void
-showHelp(char * program_name)
+void showHelp(char * program_name)
 {
   std::cout << std::endl;
   std::cout << "Usage: " << program_name << " cloud_filename.[pcd|ply]" << std::endl;
@@ -282,6 +339,9 @@ int main (int argc, char** argv)
   pcl::io::savePCDFileASCII ("testlast2.pcd", *symmetry_clouds[symmetry_clouds.size()-2]);
   pcl::io::savePCDFileASCII ("testlast.pcd", *symmetry_clouds[symmetry_clouds.size()-1]);
   
+  int lol = viewer_script(symmetry_clouds);
+  cout<<"-----------This is "<<lol<<endl;
+
   // Selecting which cloud to view and save
   pcl::PointCloud<pcl::PointXYZ>::Ptr viewer_cloud(new pcl::PointCloud<pcl::PointXYZ>);
   int cloud_number=0;
